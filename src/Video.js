fe82c5998d7d9b6d6875bdd4f6cb0fd3dfe9cd0c
Zafar201 <zafarirshad201@gmail.com>
2020-10-31 15:42:14 +0530

    initial commit

---
+++ src/Video.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Video.css'
+
+function Video() {
+    return (
+        <div className="video">
+            <video src="https://instagram.fblr4-3.fna.fbcdn.net/v/t50.2886-16/120518112_4174934222532924_2584647912123260878_n.mp4?_nc_ht=instagram.fblr4-3.fna.fbcdn.net&_nc_cat=100&_nc_ohc=ABD2xhw1bLoAX-oc0fh&oe=5F9A8329&oh=2eaf4fb474c8eab6d734ee4632b93900&dl=1">
+            </video>
+            {/* <VideoFooter/> */}
+            {/* <VideoSidebar/> */}
+
+        </div>
+    )
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZTUdw7dLpZGu-tzTdk6KtQnTPx9fFm_w",
+    authDomain: "tiktok-clone-802f7.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-802f7.firebaseio.com",
+    projectId: "tiktok-clone-802f7",
+    storageBucket: "tiktok-clone-802f7.appspot.com",
+    messagingSenderId: "16029183073",
+    appId: "1:16029183073:web:1d3723a7898ba237de8e20",
+    measurementId: "G-4ER9LWWMH9"
+  };
